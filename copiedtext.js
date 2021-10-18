6125c97028dbd8765c22afa3d76ec30619693273
MasterSawXDD <62162808+MasterSawXDD@users.noreply.github.com>
2021-10-17 20:32:52 -0400

    styles the left and right section of the main

---
+++ copiedtext.js
@@ -0,0 +1,115 @@
+// One Piece
+
+// <p class="anime-blahblah"> Detective Conan</p>
+
+// <p class="anime-blahblah"> Boruto: Naruto Next Generations</p>
+
+// <p class="anime-blahblah"> Furusato Meguri Nippon no Mukashibanashi</p>
+
+// <p class="anime-blahblah"> Dragon Ball Super Latino</p>
+
+// <p class="anime-blahblah"> Pokemon (2019)</p>
+
+// <p class="anime-blahblah"> Dragon Quest: Dai no Daibouken (2020)</p>
+
+// <p class="anime-blahblah"> Strike the Blood IV</p>
+
+// <p class="anime-blahblah"> Shaman King (2021)</p>
+
+// <p class="anime-blahblah"> Scarlet Nexus</p>
+
+// <p class="anime-blahblah"> Tsukipro The Animation 2</p>
+
+// <p class="anime-blahblah"> Shiroi Suna no Aquatope</p>
+
+// <p class="anime-blahblah"> Love Live! Superstar!!</p>
+
+// <p class="anime-blahblah"> Jahy-sama wa Kujikenai!</p>
+
+// <p class="anime-blahblah"> Getsuyoubi no Tawawa 2</p>
+
+// <p class="anime-blahblah"> Ganbare Douki-chan</p>
+
+// <pp> Heike Monogatari</pp>
+
+// <p class="anime-blahblah"> Selection Project</p>
+
+// <p class="anime-blahblah"> Yakunara Mug Cup mo: Niban Gama</p>
+
+// <p class="anime-blahblah"> Isekai Shokudou 2</p>
+
+// <p class="anime-blahblah"> Yuuki Yuuna wa Yuusha de Aru: Dai Mankai no Shou</p>
+
+// <p class="anime-blahblah"> Hanyou no Yashahime: Sengoku Otogizoushi - Ni no Shou</p>
+
+// <p class="anime-blahblah"> 86 2nd Season</p>
+
+// <p class="anime-blahblah"> Muteking the Dancing Hero</p>
+
+// <p class="anime-blahblah"> Digimon Ghost Game</p>
+
+// <p class="anime-blahblah"> Mushoku Tensei: Isekai Ittara Honki Dasu Part 2</p>
+
+// <p class="anime-blahblah"> Mieruko-chan</p>
+
+// <p class="anime-blahblah"> Sankaku Mado no Sotogawa wa Yoru</p>
+
+// <p class="anime-blahblah"> Tsuki to Laika to Nosferatu</p>
+
+// <p class="anime-blahblah"> Tesla Note</p>
+
+// <p class="anime-blahblah"> Kyuuketsuki Sugu Shinu</p>
+
+// <p class="anime-blahblah"> Shinka no Mi: Shiranai Uchi ni Kachigumi Jinsei</p>
+
+// <p class="anime-blahblah"> Kyoukai Senki</p>
+
+// <pp> Takt Op. Destiny</pp>
+
+// <p class="anime-blahblah"> Sekai Saikou no Ansatsusha, Isekai Kizoku ni Tensei suru</p>
+
+// <p class="anime-blahblah"> Shin no Nakama ja Nai to Yuusha no Party wo Oidasareta node, Henkyou de Slow Life suru Koto ni Shima</p>
+
+// <p class="anime-blahblah"> Muv-Luv Alternative</p>
+
+// <p class="anime-blahblah"> Puraore! Pride of Orange</p>
+
+// <p class="anime-blahblah"> Komi-san wa, Comyushou desu.</p>
+
+// <p class="anime-blahblah"> Sakugan</p>
+
+// <p class="anime-blahblah"> Platinum End</p>
+
+// <p class="anime-blahblah"> Visual Prison</p>
+
+// <p class="anime-blahblah"> Taishou Otome Otogibanashi</p>
+
+// <p class="anime-blahblah"> Saihate no Paladin</p>
+
+// <p class="anime-blahblah"> Build Divide: Code Black</p>
+
+// <p class="anime-blahblah"> Senpai ga Uzai Kouhai no Hanashi</p>
+
+// <p class="anime-blahblah"> World Trigger 3rd Season</p>
+
+// <p class="anime-blahblah"> Shikizakura</p>
+
+// <p class="anime-blahblah"> Kimetsu no Yaiba: Mugen Ressha-hen Arc TV</p>
+
+// <p class="anime-blahblah"> Gyakuten Sekai no Denchi Shoujo</p>
+
+// <p class="anime-blahblah"> Kaginado</p>
+
+// <p class="anime-blahblah"> Deep Insanity: The Lost Child</p>
+
+// <p class="anime-blahblah"> Ousama Ranking</p>
+
+// <p class="anime-blahblah"> Lupin III: Part 6</p>
+
+
+// // We have somethings that we need to remove. 
+// // We have to remove the words .
+// // We have to remove the text <p class="anime-blahblah">
+// // It might be different for </p>windows, but for mac
+// // if i highlight one element, and then press control + shift + l
+// // then all the elements that are similar to it will also get highlighted.
